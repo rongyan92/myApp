65e853b01b2453dba9e1cff414a33f87ef23bc93
“roongyan92” <roongyan92@163.com>
2019-07-10 16:59:16 +0800

    登录页基本布局

--- src/App.js
+++ src/App.js
@@ -4,20 +4,10 @@ import { BrowserRouter , Route, Switch,Redirect} from 'react-router-dom';
 
 // 导入semanticUI样式
 import 'semantic-ui-css/semantic.min.css'
-// 按需导入组件
-import { Button } from 'semantic-ui-react'
+
+import Login from './login'
 
 
-
-function Login(){
-  return <div>
-    <Button primary>点击</Button>
-  </div>
-}
-
-function Home (){
-  return <div>首页</div>
-}
 
 function App() {
   return (
@@ -25,7 +15,7 @@ function App() {
       {/* 路由映射 */}
       <Switch>
       <Route path='/login' component={Login}></Route>
-      <Route path='/home' component={Home}></Route>
+      {/* <Route path='/home' component={Home}></Route> */}
       <Redirect from='/' to='/login'></Redirect>
       </Switch>
     </BrowserRouter>
---
+++ src/login.js
@@ -0,0 +1,48 @@
+
+import React from 'react';
+import './login.css';
+// 按需导入组件
+import { Icon ,Form ,Button ,Divider} from 'semantic-ui-react'
+
+class Login extends React.Component {
+    render (){
+        return <div className='login_container'>
+            {/* 顶部Logo */}
+            <div className='login_logo'>
+                <Icon name='home' size='huge'></Icon>
+            </div>
+            {/* 表单区域 */}
+            <div className='login_form'>
+                <Form>
+                    <Form.Input
+                    icon='user' 
+                    required 
+                    size='big' 
+                    iconPosition='left' 
+                    name='username'
+                    placeholder='请输入用户名...' 
+                    />
+                </Form>
+                <Form>
+                    <Form.Input
+                    icon='lock' 
+                    required 
+                    size='big' 
+                    iconPosition='left' 
+                    name='password'
+                    placeholder='请输入密码...' 
+                    />
+                <Button fluid primary className='login_btn'>登录</Button>
+                </Form>
+                {/* 第三方服务 */}
+                <Divider horizontal>第三方服务</Divider>
+                <div className="login-third">
+                    <Icon size='large' blue name='qq'/>
+                    <Icon size='large' name='rocketchat'/>
+                </div>
+            </div>
+        </div>
+    }
+}
+
+export default Login
