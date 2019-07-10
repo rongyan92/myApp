629b8dfe75ba7a23979bca7b8743542f4b2ab56d
“roongyan92” <roongyan92@163.com>
2019-07-10 15:11:07 +0800

    配置全局路由，引入semanticUI组件库

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter , Route, Switch,Redirect} from 'react-router-dom';
+
+// 导入semanticUI样式
+import 'semantic-ui-css/semantic.min.css'
+// 按需导入组件
+import { Button } from 'semantic-ui-react'
+
+
+
+function Login(){
+  return <div>
+    <Button primary>点击</Button>
+  </div>
+}
+
+function Home (){
+  return <div>首页</div>
+}
 
 function App() {
   return (
-    <div className="App">
-     测试
-    </div>
+    <BrowserRouter>
+      {/* 路由映射 */}
+      <Switch>
+      <Route path='/login' component={Login}></Route>
+      <Route path='/home' component={Home}></Route>
+      <Redirect from='/' to='/login'></Redirect>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
